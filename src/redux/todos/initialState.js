67ffc850ad3b0c648191bc49a418a10a85093f32
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-03-10 15:24:36 +0600

    redux todo

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,17 @@
+export const initialState = [
+    {
+        id: 1,
+        text: 'Learn React JS',
+        completed: true,
+    },
+
+    {
+        id: 2,
+        text: 'Learn Redux',
+        completed: false,
+        color: 'red'
+    },
+
+];
+
+export default initialState;
